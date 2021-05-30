deea6773a4f9a77620412f66f389f3c55428248b
gungdekrisna <gungkrisna31@gmail.com>
2021-05-31 01:50:04 +0800

    third commit

---
+++ app/Http/Controllers/CommodityController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Commodity;
+use Illuminate\Http\Request;
+
+class CommodityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function searchCommodity(Request $request)
+    {
+        $commodity = Commodity::where('name', 'LIKE', "%" . $request->search . "%")->get();
+
+        return response()->json(['result'=> $commodity]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Commodity  $commodity
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Commodity $commodity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Commodity  $commodity
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Commodity $commodity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Commodity  $commodity
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Commodity $commodity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Commodity  $commodity
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Commodity $commodity)
+    {
+        //
+    }
+}
